9bba2e7601148ce7e11dfb991f9341603ac712f6
Dmitry Teslitsky <dmitry.teslitsky@gen.tech>
2022-07-29 14:36:32 +0300

    Build from TypeScript

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
   env: {
     browser: true,
     jquery: true,
+    node: true,
+    jest: true,
   },
   plugins: ['security'],
   extends: ['airbnb-base', 'plugin:security/recommended', 'prettier'],
---
+++ src/background.ts
@@ -0,0 +1,4 @@
+chrome.browserAction.onClicked.addListener(async () => {
+  const url = 'https://orders.gudfood.com.ua/order';
+  await chrome.tabs.create({ url, active: true });
+});
