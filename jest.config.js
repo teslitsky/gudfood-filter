03e3a26ecd1459d2ca112a6cb85aeaba493d09da
Dmitry Teslitsky <dmitry.teslitsky@gen.tech>
2023-12-09 20:48:50 +0200

    Enable V8 coverage for tests
    - extract jest config to jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: '../coverage',
+  coverageProvider: 'v8',
+  moduleFileExtensions: ['js', 'ts'],
+  rootDir: 'src',
+  testEnvironment: 'node',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+};
+
+module.exports = config;
